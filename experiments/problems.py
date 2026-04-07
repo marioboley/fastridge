b181915f9224fd314c18ea458cca2f9c0d3dad86
Mario Boley <mario.boley@gmail.com>
2026-04-07 09:07:48 +0300

    feat: add rng-threaded problems.py to experiments/

---
+++ experiments/problems.py
@@ -0,0 +1,79 @@
+import numpy as np
+from scipy.stats import wishart, multivariate_normal, uniform
+
+
+class linear_problem:
+
+    def __init__(self, beta, sigma, x_dist):
+        self.beta = beta
+        self.sigma = sigma
+        self.x_dist = x_dist
+
+    def rvs(self, number=100, rng=None):
+        if rng is None:
+            rng = np.random.default_rng()
+        x = self.x_dist.rvs(size=number, random_state=rng)
+        y = x.dot(self.beta) + rng.normal(0, self.sigma, size=number)
+        return x, y
+
+
+class eye_covariates:
+
+    def __init__(self, p):
+        self.p = p
+
+    def rvs(self, n, random_state=None):
+        I = np.eye(self.p)
+        rnd_idx = np.random.choice(self.p, size=n % self.p)
+        return np.row_stack((n // self.p * (I,) + (I[rnd_idx],)))
+
+
+class multivariate_bernoulli:
+
+    def __init__(self, probs):
+        self.probs = probs
+
+    def rvs(self, size=1, random_state=None):
+        if random_state is None:
+            random_state = np.random.default_rng()
+        res = random_state.uniform(size=(size, len(self.probs)))
+        return (res <= self.probs).astype(float)
+
+
+def random_sparse_vector(p, r, std=1, rng=None):
+    if rng is None:
+        rng = np.random.default_rng()
+    beta_ = rng.multivariate_normal(np.zeros(r), np.diag(r * [std]))
+    idx = rng.choice(p, r, replace=False)
+    beta = np.zeros(p)
+    beta[idx] = beta_
+    return beta
+
+
+def random_sparse_factor_problem(p=100, r=None, sigma_beta=1.0, sigma_eps=0.5, rng=None):
+    if rng is None:
+        rng = np.random.default_rng()
+    r = p if r is None else r
+    x_dist = multivariate_bernoulli(np.array([1 / p] * p))
+    beta = random_sparse_vector(p, r, sigma_beta, rng=rng)
+    return linear_problem(beta, sigma_eps, x_dist)
+
+
+def random_multiple_means_problem(p=100, r=None, sigma_beta=1.0, sigma_eps=0.5, rng=None):
+    if rng is None:
+        rng = np.random.default_rng()
+    r = p if r is None else r
+    x_dist = eye_covariates(p)
+    beta = random_sparse_vector(p, r, sigma_beta, rng=rng)
+    return linear_problem(beta, sigma_eps, x_dist)
+
+
+def random_problem(p, r=None, sigma_beta=1.0, sigma_eps=0.5, rng=None):
+    if rng is None:
+        rng = np.random.default_rng()
+    r = p if r is None else r
+    x_cov = wishart.rvs(p, np.eye(p), random_state=rng)
+    x_mu = rng.multivariate_normal(np.zeros(p), np.eye(p))
+    x_dist = multivariate_normal(x_mu, x_cov)
+    beta = random_sparse_vector(p, r, sigma_beta, rng=rng)
+    return linear_problem(beta, sigma_eps, x_dist)
