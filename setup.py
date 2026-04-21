b1aa60fe49b2d6c2eeb8c0d4d334d0fd0e7832be
Mario Boley <mario.boley@gmail.com>
2026-04-21 05:50:31 +0300

    chore: consolidate setup.cfg; add scikit-learn>=1.2 dependency

--- setup.py
+++ setup.py
@@ -1,18 +1,2 @@
 from setuptools import setup
-
-setup(
-    name = 'fastridge',
-    py_modules = ['fastridge'],
-    version = 'v1.1.0',
-    description = 'Fast and robust approach to ridge regression with simultaneous estimation of model parameters and hyperparameter tuning within a Bayesian framework via expectation-maximization (EM). ',
-    author = 'Mario Boley',
-    author_email = 'mario.boley@monash.edu',
-    url = 'https://github.com/marioboley/fastridge.git',
-    install_requires = ['numpy>=1.21.5', 'scipy>=1.8.1'],
-    keywords = ['Ridge regression', 'EM'],
-    classifiers = [
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+setup()
